76f125a2bb8acc8595d367dff9eeaff55f19fb32
DHATCHAYINI-2003 <108716147+DHATCHAYINI-2003@users.noreply.github.com>
2023-09-18 18:41:25 +0530

    Add files via upload

---
+++ project1/demo/src/main/java/com/project1/demo/controller/UserController.java
@@ -0,0 +1,71 @@
+package com.project1.demo.controller;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project1.demo.model.User;
+import com.project1.demo.service.UserService;
+
+@RestController
+@CrossOrigin
+public class UserController {
+    @Autowired
+     UserService userservice;
+    @GetMapping("showuser")
+    public List<User> getBookByUser() {
+        return userservice.show();
+    }
+    @GetMapping("getlogin/{emailid}/{password}")
+    public boolean login(@PathVariable String emailid,@PathVariable String password){
+    	return userservice.getsignups(emailid, password);	
+    }
+    @PostMapping("adduser")
+    public User createUser(@RequestBody User use) {
+        return userservice.addinfo(use);
+    }
+    @PutMapping("updateuser")
+    public User updateUser(@RequestBody User use) {
+    	return userservice.updateinfo(use);
+    }
+    @DeleteMapping("deleteuser/{id}")
+    public String deleteUser(@PathVariable int id) {
+    	userservice.delete(id);
+    	return "Deleted";
+    	}
+    
+	
+	@GetMapping("userSort/{field}")
+	public List<User> sort(@PathVariable String field)
+	{
+		return userservice.sort(field);
+	}
+	
+	@GetMapping("userPage/{pageno}/{pagesize}")
+	public List<User> sort(@PathVariable int pageno, @PathVariable int pagesize)
+	{
+		return userservice.page(pageno, pagesize);
+	}
+	
+	@GetMapping("userPageAndSort/{pageno}/{pagesize}/{field}")
+	public List<User> pageAndSort(@PathVariable int pageno, @PathVariable int pagesize , @PathVariable String field)
+	{
+		return userservice.pageAndSort(pageno, pagesize, field);
+	}
+	@PostMapping("add1")
+	public User saveUser(@RequestBody User user) {
+		return userservice.saveUser(user);
+	}
+	 @GetMapping("show1")
+	    public List<User> gethotelbyUsers() {
+	        return userservice.show();
+	    }
+	
+}
---
+++ project1/demo/src/main/java/com/project1/demo/model/User.java
@@ -0,0 +1,143 @@
+package com.project1.demo.model;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="users")
+public class User{
+
+	@Id
+	private int userId;
+	private String name;
+	private int age;
+	private String mail;
+	private String password;
+	private long mobileNo;
+	private long aadharNo;
+	@OneToOne(cascade = CascadeType.ALL)
+	private Hotel hotel;
+	
+	@OneToMany(cascade = CascadeType.ALL)
+	@JoinColumn(name = "fk_id")
+	private List<Booking> booking;
+	
+
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public User(int userId, String name, int age, String mail, String password, long mobileNo, long aadharNo,
+			Hotel hotel, List<Booking> booking) {
+		super();
+		this.userId = userId;
+		this.name = name;
+		this.age = age;
+		this.mail = mail;
+		this.password = password;
+		this.mobileNo = mobileNo;
+		this.aadharNo = aadharNo;
+		this.hotel = hotel;
+		this.booking = booking;
+	}
+
+
+	public int getUserId() {
+		return userId;
+	}
+
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public int getAge() {
+		return age;
+	}
+
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+
+	public String getMail() {
+		return mail;
+	}
+
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+	public long getMobileNo() {
+		return mobileNo;
+	}
+
+
+	public void setMobileNo(long mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+
+
+	public long getAadharNo() {
+		return aadharNo;
+	}
+
+
+	public void setAadharNo(long aadharNo) {
+		this.aadharNo = aadharNo;
+	}
+
+
+	public Hotel getHotel() {
+		return hotel;
+	}
+
+
+	public void setHotel(Hotel hotel) {
+		this.hotel = hotel;
+	}
+
+
+	public List<Booking> getBooking() {
+		return booking;
+	}
+
+
+	public void setBooking(List<Booking> booking) {
+		this.booking = booking;
+	}
+	
+	
+}
